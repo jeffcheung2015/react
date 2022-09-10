17dfc1a497ce7bafb8f28dae11664fc47ef1a090
Cheung jeff <cheungjeff@Cheungs-MacBook-Pro.local>
2022-09-11 00:17:24 +0800

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios/index";
+import { Button, Typography } from "@mui/material/index";
 
 function App() {
+  const [user, setUser] = useState("");
+  const [showUser, setShowUser] = useState(false);
+  useEffect(() => {
+    const run = async () => {
+      const getUserRes = await axios.get("localhost:8080/user");
+      console.log("getUserRes", getUserRes.data);
+
+      setUser(getUserRes.data.user);
+    };
+    run();
+  }, []);
+
+  const onLoginClick = () => {
+    setShowUser(true);
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          user:{" "}
+          {showUser ? (
+            <Typography>{user}</Typography>
+          ) : (
+            <Typography>Not logged in</Typography>
+          )}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button onClick={onLoginClick}>Login</Button>
       </header>
     </div>
   );
